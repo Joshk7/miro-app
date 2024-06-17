646f26be61a0a82b7b2012d622ddd28cd9d874b3
JoshK7 <joshk7683@gmail.com>
2024-06-16 21:12:31 -0500

    added view of different cursors within a room for the canvas

--- components/room.tsx
+++ components/room.tsx
@@ -6,21 +6,22 @@ import { ClientSideSuspense } from "@liveblocks/react";
 import { RoomProvider } from "@/liveblocks.config";
 
 interface RoomProps {
-    children: ReactNode,
-    roomId: string,
-    fallback: NonNullable<ReactNode> | null
+  children: ReactNode;
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
 }
 
-export const Room = ({ 
-    children,
-    roomId,
-    fallback,
- }: RoomProps) => {
+export const Room = ({ children, roomId, fallback }: RoomProps) => {
   return (
-    <RoomProvider id={roomId} initialPresence={{}}>
-        <ClientSideSuspense fallback={fallback}>
-            {() => children}
-        </ClientSideSuspense>
+    <RoomProvider
+      id={roomId}
+      initialPresence={{
+        cursor: null,
+      }}
+    >
+      <ClientSideSuspense fallback={fallback}>
+        {() => children}
+      </ClientSideSuspense>
     </RoomProvider>
   );
 };
