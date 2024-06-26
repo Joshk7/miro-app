be63296d259506a64071635f6641525544c38f1c
JoshK7 <joshk7683@gmail.com>
2024-06-26 17:21:47 -0500

    added ellipse text and notes to canvas tools

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,6 +4,9 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
+import { Text } from "./text";
+import { Ellipse } from "./ellipse";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
   id: string;
@@ -27,14 +30,42 @@ export const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <Note
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+
       case LayerType.Rectangle:
         return (
-            <Rectangle 
-                id={id}
-                layer={layer}
-                onPointerDown={onLayerPointerDown}
-                selectionColor={selectionColor}
-            />
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
         );
       default:
         // console.warn("Unknown layer type");
